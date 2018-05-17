ad407422a184c79c91eeafa3d902160ee1e18a48
amiilto <39378921+amiilto@users.noreply.github.com>
2018-05-17 15:04:14 -0300

    Adição de classes e métodos
    Adicionadas as classes principais do programa e estruturação de alguns métodos e variáveis.

---
+++ Mygle.Negocio/Models/Venda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mygle.Negocio.Models
+{
+    public class Venda
+    {
+        public long Id { get; set; }
+        public string nome { get; set; }
+        public int quantidade { get; set; }
+        public double valUnit { get; set; }
+        public double valTotal { get; set; }
+        public DateTime data { get; set; }
+
+    }
+}
---
+++ Mygle.Negocio/Models/Vendedor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mygle.Negocio.Models
+{
+    public class Vendedor
+    {
+        public void Menu()
+        {
+            string opcao = "0";
+            do
+            {
+                Console.WriteLine("1 - Cadastrar venda\n2 - Resumo\n3 - Detalhes");
+                opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "0": break;
+                    case "1":
+                        AdicionarVenda();
+                        break;
+                }
+                Console.ReadKey();
+            } while (opcao != "0");
+        }
+
+        public void AdicionarVenda()
+        {
+            Venda novaVenda = new Venda();
+
+            Console.Write("ID: ");
+            novaVenda.Id = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Nome: ");
+            novaVenda.nome = Console.ReadLine();
+
+            Console.Write("Quantidade: ");
+            novaVenda.quantidade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Valor Unitário: ");
+            novaVenda.valUnit = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Data: ");
+            //Código da data
+        }
+    }
+}

7642741691550d4c6dce14174d2b10444ff63161
amiilto <39378921+amiilto@users.noreply.github.com>
2018-05-18 09:41:18 -0300

    Adição de classes e métodos
    Adicionadas as classes principais do programa e estruturação de alguns métodos e variáveis.

--- Mygle.Telas/Program.cs
+++ Mygle.Telas/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Mygle.Negocio;
+using Mygle.Negocio.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,22 @@ namespace Mygle.Telas
     {
         static void Main(string[] args)
         {
+            Vendedor v = new Vendedor();
+            string opcao = "0";
+            do
+            {
+                Console.WriteLine("1 - Vendedor\n2 - Gerente\n");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "0": break;
+                    case "1":
+                        v.Menu();
+                        break;
+
+                }
+            } while (opcao != "0");
         }
     }
 }
